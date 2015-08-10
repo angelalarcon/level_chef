004f32885fc45e8a8c59f0ce46f23e8e556c3b49
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2015-08-10 10:05:29 -0430

    vimeo home video

--- js/main.js
+++ js/main.js
@@ -6,13 +6,6 @@ $('#carousel_courses').carousel({
   interval: 30000
 })
 
-$('.modal').click(function() {
-  $('#video_from_modal').get(0).pause()
-  if ($('body').hasClass('modal-open')) {
-    $('body').addClass('modal-close');
-  }
-});
-
 $('.play').click(function() {
   if ($('body').hasClass('modal-close')) {
     $('body').removeClass('modal-close');
