5d2cf2ba16a676c4544c484ab98c18bcd89b74b7
ELM4RC0 <maldonmarco@gmail.com>
2015-08-20 13:39:53 -0430

    color de reproductor de vimeo

--- js/main.js
+++ js/main.js
@@ -21,13 +21,16 @@ $('#menu_home').click(function(){
 });
 
 $(window).on('scroll', function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > ($('section.red').height())/2) {
-      $('#about header').addClass('colored')
-      $('#about header').removeClass('transparent')   
-    }
-    else {
-      $('#about header').removeClass('colored')
-      $('#about header').addClass('transparent')   
-    }
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop > ($('section.red').height())/2) {
+    $('#about header').addClass('colored')
+    $('#about header').removeClass('transparent')   
+  }
+  else {
+    $('#about header').removeClass('colored')
+    $('#about header').addClass('transparent')   
+  }
 });
+
+
+
